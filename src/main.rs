10ed449fd50fa90d0fcf46f2424abc52d2f7156b
Andrew Schneider <andjschneider@gmail.com>
2021-03-24 11:35:05 -0700

    Set git url to opposite style
    Updates the remote url to either SSH or HTTPS, choosing the opposite of
    what is currently configured.

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,72 @@
 use std::error::Error;
 use std::process::{Command, Stdio};
-use std::str::FromStr;
 
 #[derive(PartialEq, Default, Clone, Debug)]
 struct Remote {
     url: String,
-    style: String,     // TODO use ssh or http enum?
-    repo_name: String, // TODO what are the naming conventions?
+    ssh: bool,
+    repo_name: String,
     username: String,
 }
 
-impl FromStr for Remote {
-    type Err = std::num::ParseIntError;
+fn parse_remote(input: &str) -> Remote {
+    // check the first letter of the remote url
+    let ssh = match input.chars().nth(0).unwrap() {
+        'g' => true,
+        'h' => false,
+        _ => false, // TODO throw error
+    };
+    // TODO replace all of this with regex
+    let reversed: String = input.chars().rev().collect();
+    let first: Vec<&str> = reversed.split(".").collect();
+    let second: Vec<&str> = first[1].split("/").collect();
+    let repo = second[0];
+    let repo: String = repo.chars().rev().collect();
 
-    // git@github.com:andschneider/ac.git
-    // first: git, github.com:andschneider/ac.git
-    // second: github.com, andschneider/ac.git
-    // third: andschneider, ac.git
-    fn from_str(output: &str) -> Result<Self, Self::Err> {
-        // TODO need to check if it's ssh or https first
-        let first: Vec<&str> = output.split("@").collect();
-        let style = first[0];
-        let url = first[1];
-        let second: Vec<&str> = url.split(":").collect();
-        let third: Vec<&str> = second[1].split("/").collect();
-        Ok(Remote {
-            url: url.to_string(),
-            style: style.to_string(),
-            repo_name: third[1].to_string(),
-            username: third[0].to_string(),
-        })
+    Remote {
+        url: input.to_string(),
+        ssh: ssh,
+        repo_name: repo.to_string(),
+        username: "andschneider".to_string(), // hard coding for now
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[test]
+fn test_parse() {
+    let input = "git@github.com:andschneider/ac.git";
+    let output = parse_remote(input);
+    println!("{}", output.username);
+    assert_eq!(output.username, "andschneider");
+    assert_eq!(output.url, input);
+    assert_eq!(output.ssh, true);
+    assert_eq!(output.repo_name, "ac");
+}
+
+impl Remote {
+    fn ssh_url(&self) -> String {
+        return format!("git@github.com:{}/{}.git", self.username, self.repo_name);
+    }
+    fn https_url(&self) -> String {
+        return format!(
+            "https://github.com/{}/{}.git",
+            self.username, self.repo_name
+        );
+    }
+    fn create_url(&self) -> String {
+        match self.ssh {
+            true => self.ssh_url(),
+            false => self.https_url(),
+        }
+    }
+    fn create_url_opposite(&self) -> String {
+        match self.ssh {
+            true => self.https_url(),
+            false => self.ssh_url(),
+        }
+    }
+}
+
+fn get_git_url() -> String {
     let mut git = Command::new("git");
     let output = git
         .arg("remote")
@@ -48,21 +81,31 @@ fn main() -> Result<(), Box<dyn Error>> {
         .unwrap();
 
     let stdout = String::from_utf8(output.stdout).unwrap();
-    let remote = stdout.trim();
-    println!("{}", remote);
-    match Remote::from_str(&remote) {
-        Ok(r) => {
-            println!(
-                r"Remote style: {}, url: {}, username: {}, repo name: {}",
-                r.style, r.url, r.username, r.repo_name,
-            );
-            println!("https: https://github.com/{}/{}", r.username, r.repo_name);
-            println!("ssh: git@github.com:{}/{}", r.username, r.repo_name);
-        }
-        Err(_) => {
-            println!("{} is not a valid color hex code!", remote);
-        }
-    }
+    let stdout = stdout.trim();
+    return stdout.to_string();
+}
 
+fn set_git_url(url: &str) {
+    // git remote set-url origin git@github.com:USERNAME/REPOSITORY.git
+    let status = Command::new("git")
+        .arg("remote")
+        .arg("set-url")
+        .arg("origin")
+        .arg(url)
+        .status()
+        .expect("failed to execute process");
+
+    println!("set url exited with: {}", status);
+    assert!(status.success());
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let stdout = get_git_url();
+    let remote = parse_remote(&stdout);
+    let curr_url = remote.create_url();
+    let new_url = remote.create_url_opposite();
+    println!("{}", curr_url);
+    println!("{}", new_url);
+    set_git_url(&new_url);
     Ok(())
 }
