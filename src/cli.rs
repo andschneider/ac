39ece48bbe74fef951565cb83b0fc2ef213eed8b
Andrew Schneider <andjschneider@gmail.com>
2021-04-09 23:18:01 -0700

    Move clap app to own file
    main.rs is much cleaner now

--- src/cli.rs
+++ src/cli.rs
@@ -1,1 +1,51 @@
+use clap::{crate_version, App, AppSettings, Arg, ArgGroup, ArgMatches};
 
+pub fn create_app<'a>() -> ArgMatches<'a> {
+    App::new("ac")
+        .about("andrew's CLI")
+        .version(crate_version!())
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .subcommand(
+            App::new("git")
+                .about("some git helpers")
+                .setting(AppSettings::SubcommandRequiredElseHelp)
+                .subcommand(
+                    App::new("remote")
+                        .about("change that remote")
+                        .group(
+                            ArgGroup::with_name("remote_group")
+                                .args(&["flip", "to-ssh", "to-https"])
+                                .required(true),
+                        )
+                        .arg(
+                            Arg::with_name("flip")
+                                .short("f")
+                                .long("flip")
+                                .help("flip the remote"),
+                        )
+                        .arg(
+                            Arg::with_name("to-ssh")
+                                .short("s")
+                                .long("to-ssh")
+                                .help("change the remote to ssh"),
+                        )
+                        .arg(
+                            Arg::with_name("to-https")
+                                .short("h")
+                                .long("to-https")
+                                .help("change the remote to https"),
+                        ),
+                )
+                .subcommand(
+                    App::new("status").about("status of repos").arg(
+                        Arg::with_name("dir")
+                            .short("d")
+                            .long("dir")
+                            .takes_value(true)
+                            .help("the dir to check")
+                            .required(true),
+                    ),
+                ),
+        )
+        .get_matches()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use crate::git_status::check_git_dirs;
-use clap::{crate_version, App, AppSettings, Arg, ArgGroup, ArgMatches};
-// use std::error::Error;
-// use std::option::Option;
+use clap::ArgMatches;
 
 mod cli;
 mod git_remote;
@@ -18,60 +15,14 @@ fn handle_git_remote_command(args: &ArgMatches) {
         remote.to_https();
     }
 }
+
 fn handle_git_status_command(args: &ArgMatches) {
     let dir = args.value_of("dir").unwrap();
-    check_git_dirs(dir);
+    git_status::check_git_dirs(dir);
 }
 
 fn run() {
-    let matches = App::new("ac")
-        .about("andrew's CLI")
-        .version(crate_version!())
-        .setting(AppSettings::ArgRequiredElseHelp)
-        .subcommand(
-            App::new("git")
-                .about("some git helpers")
-                .setting(AppSettings::SubcommandRequiredElseHelp)
-                .subcommand(
-                    App::new("remote")
-                        .about("change that remote")
-                        .group(
-                            ArgGroup::with_name("remote_group")
-                                .args(&["flip", "to-ssh", "to-https"])
-                                .required(true),
-                        )
-                        .arg(
-                            Arg::with_name("flip")
-                                .short("f")
-                                .long("flip")
-                                .help("flip the remote"),
-                        )
-                        .arg(
-                            Arg::with_name("to-ssh")
-                                .short("s")
-                                .long("to-ssh")
-                                .help("change the remote to ssh"),
-                        )
-                        .arg(
-                            Arg::with_name("to-https")
-                                .short("h")
-                                .long("to-https")
-                                .help("change the remote to https"),
-                        ),
-                )
-                .subcommand(
-                    App::new("status").about("status of repos").arg(
-                        Arg::with_name("dir")
-                            .short("d")
-                            .long("dir")
-                            .takes_value(true)
-                            .help("the dir to check")
-                            .required(true),
-                    ),
-                ),
-        )
-        .get_matches();
-
+    let matches = cli::create_app();
     match matches.subcommand() {
         ("git", Some(git_matches)) => match git_matches.subcommand() {
             ("remote", Some(remote_matches)) => {
@@ -87,5 +38,6 @@ fn run() {
 }
 
 fn main() {
+    // TODO add error handling to run
     run();
 }
